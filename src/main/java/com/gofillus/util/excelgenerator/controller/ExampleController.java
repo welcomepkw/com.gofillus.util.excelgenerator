4d70a5b28b17987e4553cb0c65c2e62e0c4ce4d7
kwpark <welcomepkw@gmail.com>
2019-08-13 15:02:02 +0900

    add example

--- src/main/java/com/gofillus/util/excelgenerator/controller/ExampleController.java
+++ src/main/java/com/gofillus/util/excelgenerator/controller/ExampleController.java
@@ -1,9 +1,13 @@
 package com.gofillus.util.excelgenerator.controller;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +19,10 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gofillus.util.excelgenerator.core.ExcelGenerator;
 import com.gofillus.util.excelgenerator.exception.ExcelGenException;
+import com.gofillus.util.excelgenerator.vo.ColumnVo;
 import com.gofillus.util.excelgenerator.vo.ExcelVo;
+import com.gofillus.util.excelgenerator.vo.RowVo;
+import com.gofillus.util.excelgenerator.vo.SheetVo;
 
 @Controller
 @RequestMapping("/exam")
@@ -32,6 +39,62 @@ public class ExampleController {
 		
 		ObjectMapper mapper = new ObjectMapper();
 		ExcelVo excelVo = mapper.readValue(excelData, ExcelVo.class);
+		
+		excelGen.genExcelFile(excelVo, response);
+	}
+	
+	@RequestMapping(value = {"/test"}, method = {RequestMethod.GET})
+	public void test(
+				HttpServletResponse response
+			) throws ExcelGenException, IOException {
+		
+		String style = "rgb(100,100,100)";
+		
+		ColumnVo title1 = ColumnVo.builder()
+				.data("title1")
+				.horizontalAlignment(HorizontalAlignment.CENTER)
+				.cellType(CellType.STRING)
+				.backgroundColor(style)
+				.build();
+		ColumnVo title2 = ColumnVo.builder()
+				.data("title2")
+				.horizontalAlignment(HorizontalAlignment.CENTER)
+				.cellType(CellType.STRING)
+				.backgroundColor(style)
+				.build();
+		
+		RowVo titleRow = RowVo.builder()
+				.columns(Arrays.asList(title1, title2))
+				.build();
+		
+		
+		ColumnVo data1 = ColumnVo.builder()
+				.data("data1")
+				.horizontalAlignment(HorizontalAlignment.LEFT)
+				.cellType(CellType.STRING)
+				.build();
+		ColumnVo data2 = ColumnVo.builder()
+				.data("data2")
+				.horizontalAlignment(HorizontalAlignment.LEFT)
+				.cellType(CellType.STRING)
+				.build();
+		
+		RowVo dataRow = RowVo.builder()
+				.columns(Arrays.asList(data1, data2))
+				.build();
+				
+		
+		SheetVo sheet1 = SheetVo.builder()
+				.sheetName("sheet1")
+				.titles(titleRow)
+				.datas(Arrays.asList(dataRow))
+				.build();
+		
+		ExcelVo excelVo = ExcelVo.builder()
+				.fileName("test.xls")
+				.workbookType(HSSFWorkbook.class)
+				.sheets(Arrays.asList(sheet1))
+				.build();
 		
 		excelGen.genExcelFile(excelVo, response);
 	}
